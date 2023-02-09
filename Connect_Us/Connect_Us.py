8964c8847f13fa878d70013125a4e8229357b2d1
Advik <advik.b@gmail.com>
2023-02-09 17:26:39 +0530

    Update Connect_Us.py

--- Connect_Us/Connect_Us.py
+++ Connect_Us/Connect_Us.py
@@ -2,6 +2,7 @@
 from pcconfig import config
 
 import pynecone as pc
+from .index import index
 
 docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -13,29 +14,6 @@ class State(pc.State):
     pass
 
 
-def index():
-    return pc.center(
-        pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-        ),
-        padding_top="10%",
-    )
-
-
-# Add state and page to the app.
 app = pc.App(state=State)
 app.add_page(index)
 app.compile()
