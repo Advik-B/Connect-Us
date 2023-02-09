294b5b69d013d862e1cacb8d015fb10f7861b275
Advik <advik.b@gmail.com>
2023-02-09 17:29:54 +0530

    Update index.py

--- Connect_Us/index.py
+++ Connect_Us/index.py
@@ -1,10 +1,11 @@
 from pynecone import text, center
 
+
 def index():
     return center(
         text(
             "Hello world!",
-            font_size="2rem"
+            font_size="2rem",
+            font_weight="bold",
         )
     )
-
