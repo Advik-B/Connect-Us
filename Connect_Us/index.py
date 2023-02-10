c415421cbd230d4337898b8bef6396c400e5e084
Advik <advik.b@gmail.com>
2023-02-10 07:38:35 +0530

    Update index.py

--- Connect_Us/index.py
+++ Connect_Us/index.py
@@ -1,11 +1,24 @@
-from pynecone import text, center
-
+from pynecone import *
+from .sidebar import sidebar
 
 def index():
-    return center(
-        text(
-            "Hello world!",
-            font_size="2rem",
-            font_weight="bold",
+    return box(
+        sidebar(),
+        vstack(
+            text(
+                "Welcome to Connect Us!",
+                font_size="1.5rem",
+                font_family="JetBrains Mono",
+                font_weight="bold",
+            ),
+            text(
+                "This is a simple app that allows you to connect with other people.",
+                font_size="1rem",
+                font_family="JetBrains Mono",
+                font_weight="bold",
+            ),
         )
     )
+
+
+
